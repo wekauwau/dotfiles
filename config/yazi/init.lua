73183e770551caa8317ac9ebc35855d68a806345
wekauwau <wekauwau@gmail.com>
2024-12-06 08:26:15 +0700

    chore: comment unused

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -3,12 +3,12 @@ require("full-border"):setup({
 	type = ui.Border.ROUNDED,
 })
 
-Header:children_add(function()
-	if ya.target_family() ~= "unix" then
-		return ui.Line({})
-	end
-	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("magenta")
-end, 500, Header.LEFT)
+-- Header:children_add(function()
+-- 	if ya.target_family() ~= "unix" then
+-- 		return ui.Line({})
+-- 	end
+-- 	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("magenta")
+-- end, 500, Header.LEFT)
 
 require("git"):setup()
 
