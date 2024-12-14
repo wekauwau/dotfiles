7b6963e44e1da09dbe24b5d1dd380b024f6cbafd
wekauwau <wekauwau@gmail.com>
2024-12-14 14:18:00 +0700

    lazy.nvim update

--- config/nvim/lua/plugins/nvim-lualine.lua
+++ config/nvim/lua/plugins/nvim-lualine.lua
@@ -11,11 +11,6 @@ return {
       vim.o.laststatus = vim.g.lualine_laststatus
 
       local opts = {
-        options = {
-          theme = "auto",
-          globalstatus = vim.o.laststatus == 3,
-          disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter" } },
-        },
         sections = {
           lualine_a = { "mode" },
           lualine_b = { "branch" },
@@ -35,32 +30,30 @@ return {
             { LazyVim.lualine.pretty_path() },
           },
           lualine_x = {
-            "filetype",
-          },
-          lualine_y = {
+            Snacks.profiler.status(),
         -- stylua: ignore
         {
           function() return require("noice").api.status.command.get() end,
           cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-          color = function() return LazyVim.ui.fg("Statement") end,
+          color = function() return { fg = Snacks.util.color("Statement") } end,
         },
         -- stylua: ignore
         {
           function() return require("noice").api.status.mode.get() end,
           cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-          color = function() return LazyVim.ui.fg("Constant") end,
+          color = function() return { fg = Snacks.util.color("Constant") } end,
         },
         -- stylua: ignore
         {
           function() return "  " .. require("dap").status() end,
           cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
-          color = function() return LazyVim.ui.fg("Debug") end,
+          color = function() return { fg = Snacks.util.color("Debug") } end,
         },
         -- stylua: ignore
         {
           require("lazy.status").updates,
           cond = require("lazy.status").has_updates,
-          color = function() return LazyVim.ui.fg("Special") end,
+          color = function() return { fg = Snacks.util.color("Special") } end,
         },
             {
               "diff",
@@ -81,33 +74,17 @@ return {
               end,
             },
           },
+          lualine_y = {
+            { "progress", separator = " ", padding = { left = 1, right = 0 } },
+            { "location", padding = { left = 0, right = 1 } },
+            { "filetype" },
+          },
           lualine_z = {
             { "progress", separator = " ", padding = { left = 1, right = 0 } },
             { "location", padding = { left = 0, right = 1 } },
           },
         },
-        extensions = { "neo-tree", "lazy" },
       }
-
-      -- do not add trouble symbols if aerial is enabled
-      -- And allow it to be overriden for some buffer types (see autocmds)
-      if vim.g.trouble_lualine and LazyVim.has("trouble.nvim") then
-        local trouble = require("trouble")
-        local symbols = trouble.statusline({
-          mode = "symbols",
-          groups = {},
-          title = false,
-          filter = { range = true },
-          format = "{kind_icon}{symbol.name:Normal}",
-          hl_group = "lualine_c_normal",
-        })
-        table.insert(opts.sections.lualine_c, {
-          symbols and symbols.get,
-          cond = function()
-            return vim.b.trouble_lualine ~= false and symbols.has()
-          end,
-        })
-      end
 
       return opts
     end,
