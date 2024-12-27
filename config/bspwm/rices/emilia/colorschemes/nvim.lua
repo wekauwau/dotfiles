604d10e59d711f40f2411f3a74e0056f7eeeba7e
wekauwau <wekauwau@gmail.com>
2024-12-27 18:18:23 +0700

    simplify nvim colorscheme for emilia
    Tokyonight colorscheme is installed by default in lazyvim

--- config/bspwm/rices/emilia/colorschemes/nvim.lua
+++ config/bspwm/rices/emilia/colorschemes/nvim.lua
@@ -1,20 +1,20 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		name = "tokyonight",
-		opts = {
-			style = "night",
-			transparent = true,
-			on_colors = function(colors)
-				colors.border = "#565f89"
-			end,
-		},
-		dim_inactive = true,
-	},
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	name = "tokyonight",
+	-- 	opts = {
+	-- 		style = "night",
+	-- 		transparent = true,
+	-- 		on_colors = function(colors)
+	-- 			colors.border = "#565f89"
+	-- 		end,
+	-- 	},
+	-- 	dim_inactive = true,
+	-- },
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "tokyonight",
+			colorscheme = "tokyonight-night",
 		},
 	},
 }
