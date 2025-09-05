474d67b0ce1bae89ce4a8db68be0f7a621764486
wekauwau <wekauwau@gmail.com>
2025-09-06 05:59:08 +0700

    fix: clara:nvim colorscheme

--- config/bspwm/rices/clara/colorschemes/nvim.lua
+++ config/bspwm/rices/clara/colorschemes/nvim.lua
@@ -2,7 +2,20 @@ return {
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "tokyonight-day",
+			colorscheme = "catppuccin",
+			-- Add bufferline integration config
+			bufferline = {
+				highlights = require("catppuccin.groups.integrations.bufferline").get_theme(),
+			},
+		},
+		-- Ensure catppuccin loads before bufferline
+		dependencies = {
+			{
+				"akinsho/bufferline.nvim",
+				opts = function(_, opts)
+					opts.highlights = require("catppuccin.groups.integrations.bufferline").get_theme()
+				end,
+			},
 		},
 	},
 }
