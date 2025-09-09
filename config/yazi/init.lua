3f62ef0ccf0db21c5faaf66d6f806b40f1c6c209
wekauwau <wekauwau@gmail.com>
2025-09-10 00:13:28 +0700

    feat: implement yazi docs tips

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -1,3 +1,14 @@
+-- Show symlink in status bar
+-- https://yazi-rs.github.io/docs/tips#symlink-in-status
+Status:children_add(function(self)
+	local h = self._current.hovered
+	if h and h.link_to then
+		return " -> " .. tostring(h.link_to)
+	else
+		return ""
+	end
+end, 3300, Status.LEFT)
+
 require("full-border"):setup({
 	type = ui.Border.ROUNDED,
 })
